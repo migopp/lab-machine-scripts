b21c38acf4b39cf5f8440338a0b6bb8df2a775a4
Michael Goppert <goppert@utexas.edu>
2025-02-14 23:05:37 -0600

    os-migrate

---
+++ os-migrate.lua
@@ -0,0 +1,12 @@
+-- os-migrate
+--
+-- A simple lua script to migrate code from one project to the next
+-- in the kernels of Dr. Gheith's CS 439 class
+--
+-- written by goppert
+
+local function migrate()
+	print("hello")
+end
+
+migrate()
